afac42e34b81fb0c752e71ec82b3f52242a1e77b
Dana <dano4ka1007@gmail.com>
2023-05-01 17:22:24 +0300

    change design and photoView instead of imageView

--- app/src/main/java/com/example/spaceapp/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/spaceapp/ui/start/StartFragment.kt
@@ -1,16 +1,16 @@
 package com.example.spaceapp.ui.start
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.example.spaceapp.SpaceApp
 import com.example.spaceapp.databinding.FragmentStartBinding
 import com.example.spaceapp.di.ViewModelFactory
+import com.github.chrisbanes.photoview.PhotoViewAttacher
 import javax.inject.Inject
 
 class StartFragment : Fragment() {
@@ -34,12 +34,21 @@ class StartFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val photoView = binding.DayPictureImg
+
+        val attacher = PhotoViewAttacher(photoView)
+        attacher.isZoomable = true
+
         viewModel.dayPictureLiveData.observe(viewLifecycleOwner){
             Glide
                 .with(view)
                 .load(it.url)
                 .into(binding.DayPictureImg)
+            binding.PictureTitleTv.text = it.title
+            binding.PictureDateTv.text = it.data
+            binding.PictureExplanationTv.text = it.explanation
         }
 
         viewModel.getDayPicture()
